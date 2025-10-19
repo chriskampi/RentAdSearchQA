0994aecfb9c0ab0b0e4b6c42b421e3aae1a7ad02
Your Name <your.email@example.com>
2025-10-19 19:55:42 +0300

    fix

---
+++ src/main/java/com/rentadsearchqa/locators/pages/PropertyPage.java
@@ -0,0 +1,22 @@
+package com.rentadsearchqa.locators.pages;
+
+import com.rentadsearchqa.locators.components.SearchPage;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Property page class that extends SearchPage and uses only
+ * the paths and functionality from SearchPage via super.
+ */
+public class PropertyPage extends SearchPage {
+
+    // ===== CONSTRUCTOR =====
+    public PropertyPage(WebDriver driver) {
+        super(driver);
+    }
+
+    // ===== TYPE FUNCTION =====s
+    public void typeAreaSearchInput(String text) {
+        typeSearchInput("area", text);
+    }
+
+}
